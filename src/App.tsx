a97b4198971c81519f16450ef23f551a8fae7961
AzrilYusuf <muhammadazrilyusuf.w3rk@gmail.com>
2025-11-22 15:34:52 +0700

    feat: Learn - Leva (helper). - Lighting (pointLight,ambientLight, spotlight, and directionalLight). - Light helper. - Shadowing (cast shadow and receive shadow). - Import .glb file and render it into canvas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import { useRef } from "react";
-import { Mesh, Texture, TextureLoader } from "three";
+import {
+  Mesh,
+  Texture,
+  TextureLoader,
+  // SpotLightHelper,
+  // Object3D
+} from "three";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import {
   OrbitControls,
   GizmoHelper,
   GizmoViewcube,
   GizmoViewport,
+  // useHelper,
+  useGLTF,
 } from "@react-three/drei";
+// import { useControls } from "leva";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import image from "./assets/react.svg";
 import "./index.css";
+
+function LightWithHelper(): React.JSX.Element {
+  // const lightRef = useRef<Object3D | null>(null);
+
+  // useHelper(lightRef, SpotLightHelper, "white");
+
+  return (
+    <>
+      /* A spotlight with a helper to visualize its position and direction
+      penumbral angle is set to 1 for a soft edge */
+      {/* <spotLight
+        ref={lightRef}
+        intensity={90}
+        position={[2, 5, 1]}
+        angle={Math.PI / 5}
+        penumbra={0.2}
+        castShadow
+      /> */}
+      <directionalLight position={[1, 1, 2]} />
+      {/* <ambientLight intensity={2} /> */}
+      {/* <pointLight intensity={50} position={[2,5,1]} /> */}
+    </>
+  );
+}
+
+function Room(): React.JSX.Element {
+  const result = useGLTF("/room-studio.glb");
+  return <primitive object={result.scene} />;
+}
 
 // The box will rotate on each frame
 function AnimatedBox(): React.JSX.Element {
@@ -16,6 +55,16 @@ function AnimatedBox(): React.JSX.Element {
   const boxRef = useRef<Mesh | null>(null);
   // Load the texture
   const texture: Texture<HTMLImageElement> = useLoader(TextureLoader, image);
+
+  // Leva controls for speed
+  // const { speed }: { speed: number } = useControls({
+  //   speed: {
+  //     value: 0.0025,
+  //     min: 0.0,
+  //     max: 0.01,
+  //     step: 0.0005,
+  //   }
+  // });
 
   // Rotate the box on each frame
   useFrame(() => {
@@ -26,9 +75,9 @@ function AnimatedBox(): React.JSX.Element {
   });
 
   return (
-    <mesh ref={boxRef}>
+    <mesh ref={boxRef} castShadow>
       <boxGeometry args={[3, 3, 3]} />
-      <meshBasicMaterial map={texture} />
+      <meshStandardMaterial map={texture} />
     </mesh>
   );
 }
@@ -36,7 +85,7 @@ function AnimatedBox(): React.JSX.Element {
 function App(): React.JSX.Element {
   return (
     <div id="canvas-container">
-      <Canvas className="canvas">
+      <Canvas className="canvas" shadows>
         {/* Helper */}
         <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
           <GizmoViewcube />
@@ -49,8 +98,17 @@ function App(): React.JSX.Element {
         <gridHelper args={[20, 30]} />
         {/* Orbit Camera */}
         <OrbitControls />
+        <LightWithHelper />
         <AnimatedBox />
-        <directionalLight position={[1, 1, 2]} />
+        <mesh
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -3, 0]}
+          receiveShadow
+        >
+          <planeGeometry args={[100, 100]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+        <Room />
       </Canvas>
     </div>
   );
